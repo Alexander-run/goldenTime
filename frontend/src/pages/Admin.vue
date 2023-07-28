11f485fb06c66515e9b805563d321779f8846857
Alexander-run <advance@AdvancedeMacBook-Pro.local>
2023-07-28 14:26:10 +0800

    u

--- frontend/src/pages/Admin.vue
+++ frontend/src/pages/Admin.vue
@@ -7,7 +7,7 @@ import {
 } from 'vue'
 import axios from 'axios'
 import * as XLSX from 'xlsx'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 declare module 'element-plus' {
   export const ElMessage: any
 }
@@ -24,17 +24,67 @@ interface JourneyTableItem {
   id: number,
   depatureDate: string,
   fee: number,
+  depaturePositionName: string,
+  destinationPositionName: string,
+  leaderName: string,
+  leaderPhone: string,
+  description: string,
 }
 const activeTab = ref('users')
-const isAdmin = ref(false)
+const isAdmin = ref(sessionStorage.getItem('password') && sessionStorage.getItem('password') === 'woshixiaopacai')
 const password = ref('')
+const activePage = ref('journeyTable')
 const userTableData: Ref<UserTableItem[]> = ref([])
 const journeyTableData: Ref<JourneyTableItem[]> = ref([])
-const journeyForm: Ref<JourneyTableItem> = ref({
+const newJourneyForm: Ref<JourneyTableItem> = ref({
   id: 0,
   depatureDate: '',
   fee: 0,
+  depaturePositionName: '',
+  destinationPositionName: '丰宁大滩镇',
+  leaderName: '',
+  leaderPhone: '',
+  description: '',
 })
+const selectedJourneyForm: Ref<JourneyTableItem> = ref({
+  id: 0,
+  depatureDate: '',
+  fee: 0,
+  depaturePositionName: '',
+  destinationPositionName: '丰宁大滩镇',
+  leaderName: '',
+  leaderPhone: '',
+  description: '',
+})
+const createFormRef = ref<FormInstance>()
+const editFormRef = ref<FormInstance>()
+const rules = {
+  depatureDate: [
+    { required: true, message: '请选择日期', trigger: 'blur' },
+  ],
+  fee: [
+    { required: true, message: '请输入费用', trigger: 'blur' },
+  ],
+  depaturePositionName: [
+    { required: true, message: '请选择出发地', trigger: 'blur' },
+  ],
+  destinationPositionName: [
+    { required: true, message: '请选择目的地', trigger: 'blur' },
+  ],
+  leaderName: [
+    { required: true, message: '请选择领队姓名', trigger: 'blur' },
+  ],
+  leaderPhone: [
+    { required: true, message: '请输入领队电话', trigger: 'blur' },
+  ],
+  description: [
+    { required: true, message: '请输入行程描述', trigger: 'blur' },
+  ],
+}
+const handleClickBtnEditJourney = (row: JourneyTableItem) => {
+  selectedJourneyForm.value = row
+  activePage.value = 'edit'
+}
 const initTable = () => {
   axios.get('/api/cases/all').then((res: any) => {
     userTableData.value = res.data
@@ -47,38 +97,60 @@ const handleDeleteUser = (row: UserTableItem) => {
   const params = {
     id: row.id,
   }
-  axios.post('/api/cases/delete', params).then((res: any) => {
-    if (res.status.toString()[0] == 2) {
-      ElMessage({
-        message: '删除成功',
-        type: 'success',
-      })
-    } else {
-      ElMessage({
-        message: '删除失败',
-        type: 'error',
-      })
-    }
-    initTable()
+  ElMessageBox.confirm('确认删除该用户吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  }).then(() => {
+    axios.post('/api/cases/delete', params).then((res: any) => {
+      if (res.status.toString()[0] == 2) {
+        ElMessage({
+          message: '删除成功',
+          type: 'success',
+        })
+      } else {
+        ElMessage({
+          message: '删除失败',
+          type: 'error',
+        })
+      }
+      initTable()
+    })
+  }).catch(() => {
+    ElMessage({
+      type: 'info',
+      message: '已取消删除',
+    })
   })
 }
 const handleDeleteJourney = (row: JourneyTableItem) => {
   const params = {
     id: row.id,
   }
-  axios.post('/api/journeys/delete', params).then((res: any) => {
-    if (res.status.toString()[0] == 2) {
-      ElMessage({
-        message: '删除成功',
-        type: 'success',
-      })
-    } else {
-      ElMessage({
-        message: '删除失败',
-        type: 'error',
-      })
-    }
-    initTable()
+  ElMessageBox.confirm('确认删除该行程吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  }).then(() => {
+    axios.post('/api/journeys/delete', params).then((res: any) => {
+      if (res.status.toString()[0] == 2) {
+        ElMessage({
+          message: '删除成功',
+          type: 'success',
+        })
+      } else {
+        ElMessage({
+          message: '删除失败',
+          type: 'error',
+        })
+      }
+      initTable()
+    })
+  }).catch(() => {
+    ElMessage({
+      type: 'info',
+      message: '已取消删除',
+    })
   })
 }
 const handleCheckedStatusChange = (row: UserTableItem) => {
@@ -97,20 +169,60 @@ const handleCheckedStatusChange = (row: UserTableItem) => {
     initTable()
   })
 }
-const handleCreateJourney = () => {
-  axios.post('/api/journeys/create', journeyForm.value).then((res: any) => {
-    if (res.status.toString()[0] == 2) {
-      ElMessage({
-        message: '创建成功',
-        type: 'success',
+const handleCreateJourney = async() => {
+  if (!createFormRef.value) return
+  await createFormRef.value.validate((valid) => {
+    if (valid) {
+      axios.post('/api/journeys/create', newJourneyForm.value).then((res: any) => {
+        if (res.status.toString()[0] == 2) {
+          ElMessage({
+            message: '创建成功',
+            type: 'success',
+          })
+          activePage.value = 'journeyTable'
+        } else {
+          ElMessage({
+            message: '创建失败',
+            type: 'error',
+          })
+        }
+        initTable()
       })
     } else {
       ElMessage({
-        message: '创建失败',
+        message: '请检查表单',
         type: 'error',
       })
+      return false
     }
-    initTable()
+  })
+}
+const handleEditJourney = async() => {
+  if (!editFormRef.value) return
+  await editFormRef.value.validate((valid) => {
+    if (valid) {
+      axios.post('/api/journeys/update', selectedJourneyForm.value).then((res: any) => {
+        if (res.status.toString()[0] == 2) {
+          ElMessage({
+            message: '更新成功',
+            type: 'success',
+          })
+          activePage.value = 'journeyTable'
+        } else {
+          ElMessage({
+            message: '更新失败',
+            type: 'error',
+          })
+        }
+        initTable()
+      })
+    } else {
+      ElMessage({
+        message: '请检查表单',
+        type: 'error',
+      })
+      return false
+    }
   })
 }
 const handleDownload = () => {
@@ -134,6 +246,17 @@ const handleDownload = () => {
     type: 'success',
   })
 }
+const handleLogin = () => {
+  if (password.value === 'woshixiaopacai') {
+    isAdmin.value = true
+    sessionStorage.setItem('password', 'woshixiaopacai')
+  } else {
+    ElMessage({
+      message: '接头暗号错误',
+      type: 'error',
+    })
+  }
+}
 onMounted(() => {
   initTable()
 })
@@ -141,76 +264,181 @@ onMounted(() => {
 <template>
   <div class="user">
     <h2>Admin</h2>
-    <el-form class="form" v-if="!isAdmin" @submit.prevent="isAdmin = password === 'woshixiaopacai'">
+    <el-form class="form" v-if="!isAdmin" @submit.prevent="handleLogin">
       <el-form-item label="接头暗号">
         <el-input v-model="password" placeholder="接头暗号" type="password" />
       </el-form-item>
       <el-form-item>
-        <el-button @click="isAdmin = password === 'woshixiaopacai'" type="primary">登录</el-button>
+        <el-button @click="handleLogin" type="primary">登录</el-button>
       </el-form-item>
     </el-form>
     <div v-else>
       <el-tabs v-model="activeTab" class="tabs">
         <el-tab-pane label="登记用户管理" name="users">
-          <el-button @click="handleDownload" type="primary">下载所有信息(xlsx)</el-button>
-          <el-table :data="userTableData" class="table">
-            <el-table-column prop="name" label="姓名" />
-            <el-table-column prop="depatureDate" label="出发日期" width="110" />
-            <el-table-column prop="headCount" label="出行人数" width="80" />
-            <el-table-column prop="description" label="备注" width="200" />
-            <el-table-column prop="isCheckedIn" label="已交定金" width="80">
-              <template #default="{ row }">
-                <el-switch @change="handleCheckedStatusChange(row)" v-model="row.isCheckedIn" />
-              </template>
-            </el-table-column>
-            <el-table-column width="50">
-              <template #default="{ row }">
-                <el-button @click="handleDeleteUser(row)" type="danger" size="small">X</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
+          <div :style="{ marginBottom: '40px' }">
+            <el-button @click="handleDownload" type="primary">下载所有信息(xlsx)</el-button>
+            <el-table :data="userTableData" class="table">
+              <el-table-column prop="name" label="姓名" />
+              <el-table-column prop="depatureDate" label="出发日期" width="110" />
+              <el-table-column prop="headCount" label="出行人数" width="80" />
+              <el-table-column prop="description" label="备注" width="200" />
+              <el-table-column prop="isCheckedIn" label="已交定金" width="80">
+                <template #default="{ row }">
+                  <el-switch @change="handleCheckedStatusChange(row)" v-model="row.isCheckedIn" />
+                </template>
+              </el-table-column>
+              <el-table-column width="50">
+                <template #default="{ row }">
+                  <el-button @click="handleDeleteUser(row)" type="danger" size="small">X</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
         </el-tab-pane>
         <el-tab-pane label="活动管理" name="journeys">
-          <el-form :model="journeyForm">
-            <el-form-item label="出发日期" prop="depatureDate">
-              <el-date-picker
-                v-model="journeyForm.depatureDate"
-                type="date"
-                placeholder="选择日期"
-                format="YYYY-MM-DD"
-                value-format="YYYY-MM-DD">
-              </el-date-picker>
-            </el-form-item>
-            <el-form-item label="费用" prop="fee">
-              <el-input-number v-model="journeyForm.fee" :min="0" :max="100000" label="费用/人" />
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="handleCreateJourney">发布活动</el-button>
-            </el-form-item>
-          </el-form>
-          <el-table :data="journeyTableData" class="table">
-            <el-table-column prop="depatureDate" label="出发日期" />
-            <el-table-column prop="fee" label="费用/人">
-              <template #default="{ row }">
-                {{ row.fee }}元
-              </template>
-            </el-table-column>
-            <el-table-column width="50">
-              <template #default="{ row }">
-                <el-button @click="handleDeleteJourney(row)" type="danger" size="small">X</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
+          <div class="tab-container">
+            <el-form :model="newJourneyForm" class="journey-form sub-tab" :rules="rules" ref="createFormRef" v-if="activePage==='create'">
+              <div class="title-with-route">
+                <el-button @click="activePage='journeyTable'" type="primary">返回</el-button>
+                <h3>发布新活动</h3>
+              </div>
+              <el-form-item label="出发日期" prop="depatureDate">
+                <el-date-picker
+                  v-model="newJourneyForm.depatureDate"
+                  type="date"
+                  placeholder="选择日期"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD">
+                </el-date-picker>
+              </el-form-item>
+              <el-form-item label="费用/人" prop="fee">
+                <el-input-number v-model="newJourneyForm.fee" :min="0" :max="100000" label="费用/人" />
+              </el-form-item>
+              <el-form-item label="领队" prop="leaderName">
+                <el-select v-model="newJourneyForm.leaderName" placeholder="选择领队">
+                  <el-option label="曹旭" value="曹旭" />
+                  <el-option label="暴依宁" value="暴依宁" />
+                  <el-option label="郭彪" value="郭彪" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="联系电话" prop="leaderPhone">
+                <el-input v-model="newJourneyForm.leaderPhone" placeholder="联系电话" />
+              </el-form-item>
+              <el-form-item label="集合地点" prop="depaturePositionName">
+                <el-select placeholder="选择集合地点" v-model="newJourneyForm.depaturePositionName">
+                  <el-option label="望京华彩" value="望京华彩" />
+                  <el-option label="海淀清河" value="海淀清河" />
+                  <el-option label="通州梨园" value="通州梨园" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="目的地" prop="destinationPositionName">
+                <el-select disabled placeholder="选择目的地" v-model="newJourneyForm.destinationPositionName">
+                  <el-option label="丰宁大滩镇" value="丰宁大滩镇" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="活动安排" prop="description">
+                <el-input rows="6" type="textarea" placeholder="活动详情" v-model="newJourneyForm.description" />
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="handleCreateJourney">发布新活动</el-button>
+              </el-form-item>
+            </el-form>
+            <div v-else-if="activePage==='journeyTable'" class="sub-tab">
+              <div class="title-with-route">
+                <h3>已发布的活动</h3>
+                <el-button @click="activePage = 'create'" type="primary">发布一个新活动</el-button>
+              </div>
+              <el-table :data="journeyTableData" class="table">
+                <el-table-column prop="depatureDate" width="110" label="出发日期" />
+                <el-table-column prop="depaturePositionName" label="集合地点" />
+                <el-table-column prop="leaderName" label="领队" />
+                <el-table-column prop="fee" label="费用/人">
+                  <template #default="{ row }">
+                    {{ row.fee }}元
+                  </template>
+                </el-table-column>
+                <el-table-column width="120">
+                  <template #default="{ row }">
+                    <el-button @click="handleClickBtnEditJourney(row)" type="primary" size="small">
+                      <el-icon>
+                        <Edit />
+                      </el-icon>
+                    </el-button>
+                    <el-button @click="handleDeleteJourney(row)" type="danger" size="small">
+                      <el-icon>
+                        <Delete />
+                      </el-icon>
+                    </el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
+            <el-form :model="selectedJourneyForm" class="journey-form sub-tab" :rules="rules" ref="editFormRef" v-else-if="activePage==='edit'">
+              <div class="title-with-route">
+                <el-button @click="activePage='journeyTable'" type="primary">返回</el-button>
+                <h3>编辑此活动</h3>
+              </div>
+              <el-form-item label="出发日期" prop="depatureDate">
+                <el-date-picker
+                  v-model="selectedJourneyForm.depatureDate"
+                  type="date"
+                  placeholder="选择日期"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD">
+                </el-date-picker>
+              </el-form-item>
+              <el-form-item label="费用/人" prop="fee">
+                <el-input-number v-model="selectedJourneyForm.fee" :min="0" :max="100000" label="费用/人" />
+              </el-form-item>
+              <el-form-item label="领队" prop="leaderName">
+                <el-select v-model="selectedJourneyForm.leaderName" placeholder="选择领队">
+                  <el-option label="曹旭" value="曹旭" />
+                  <el-option label="暴依宁" value="暴依宁" />
+                  <el-option label="郭彪" value="郭彪" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="联系电话" prop="leaderPhone">
+                <el-input v-model="selectedJourneyForm.leaderPhone" placeholder="联系电话" />
+              </el-form-item>
+              <el-form-item label="集合地点" prop="depaturePositionName">
+                <el-select placeholder="选择集合地点" v-model="selectedJourneyForm.depaturePositionName">
+                  <el-option label="望京华彩" value="望京华彩" />
+                  <el-option label="海淀清河" value="海淀清河" />
+                  <el-option label="通州梨园" value="通州梨园" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="目的地" prop="destinationPositionName">
+                <el-select disabled placeholder="选择目的地" v-model="selectedJourneyForm.destinationPositionName">
+                  <el-option label="丰宁大滩镇" value="丰宁大滩镇" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="活动安排" prop="description">
+                <el-input rows="6" type="textarea" placeholder="活动详情" v-model="selectedJourneyForm.description" />
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="handleEditJourney">确认修改</el-button>
+              </el-form-item>
+            </el-form>
+          </div>
         </el-tab-pane>
       </el-tabs>
     </div>
   </div>
 </template>
 <style lang="less" scoped>
+.title-with-route {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  button {
+    margin: 16px;
+  }
+}
 .user {
   display: flex;
   flex-direction: column;
   align-items: center;
+  // background: lightgrey;
   .form {
     display: flex;
     flex-direction: column;
@@ -219,6 +447,23 @@ onMounted(() => {
   .table {
     width: 100%;
     margin-top: 20px;
+  }
+}
+.tab-container {
+  width: 95%;
+  display: flex;
+  flex-direction: row;
+  .sub-tab {
+    width: 100%;
+  }
+  .journey-form {
+    margin-right: 40px;
+    :deep(.el-form-item__content) {
+      display: flex;
+      flex-direction: row;
+      justify-content: right;
+      align-items: center;
+    }
   }
 }
 </style>
--- frontend/src/pages/User.vue
+++ frontend/src/pages/User.vue
@@ -8,7 +8,7 @@ import {
 } from 'vue'
 import axios from 'axios'
 import moment from 'moment'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 declare module 'element-plus' {
   export const ElMessage: any
 }
@@ -29,70 +29,106 @@ const formData: Ref<FormData> = ref({
   headCount: 0,
   description: '',
 })
-const availableDates = ref([])
+const selectedJourney = ref({})
+const availableJourneys = ref([])
 const selectedFee = ref(0)
+const userFormRef = ref<FormInstance>()
+const rules = {
+  name: [
+    { required: true, message: '请输入姓名', trigger: 'blur' },
+  ],
+  depatureDate: [
+    { required: true, message: '请选择日期', trigger: 'blur' },
+  ],
+  headCount: [
+    { required: true, message: '请输入人数', trigger: 'blur' },
+  ],
+}
 const handleDateChange = (e) => {
-  const target = availableDates.value.find((item: any) => {
+  const target = availableJourneys.value.find((item: any) => {
     return item.depatureDate === e
   })
   if (target) {
     selectedFee.value = target.fee
+    selectedJourney.value = target
   }
 }
-const onSubmit = () => {
-  // TODO post update to backend
-  axios.post('/api/cases/create', formData.value).then((res: any) => {
-    if (res.status.toString()[0] == 2) {
-      ElMessage({
-        message: '创建成功',
-        type: 'success',
-      })
-    } else {
-      ElMessage({
-        message: '创建失败',
-        type: 'error',
+const onSubmit = async() => {
+  if(!userFormRef.value) return
+  await userFormRef.value.validate((valid: boolean) => {
+    if (valid) {
+      ElMessageBox.confirm('您每天只有三次报名机会，请确认信息是否正确，确认报名吗？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        axios.post('/api/cases/create', formData.value).then((res: any) => {
+          if (res.status.toString()[0] == 2) {
+            ElMessage({
+              message: '报名成功',
+              type: 'success',
+            })
+          } else {
+            ElMessage({
+              message: '失败了，请稍后重试',
+              type: 'error',
+            })
+          }
+        })
       })
     }
   })
+  
 }
-const initAvailableDates = () => {
+const initAvailableJourneys = () => {
   axios.get('/api/journeys/all').then((res: any) => {
     if (res.status.toString()[0] == 2) {
-      availableDates.value = res.data
+      availableJourneys.value = res.data
     }
   })
 }
 const disabledDate = (time: Date) => {
   const temp = moment(time).format('YYYY-MM-DD')
-  return !availableDates.value.some((item: any) => {
+  return !availableJourneys.value.some((item: any) => {
     return item.depatureDate === temp
   })
 }
 onMounted(() => {
-  initAvailableDates()
+  initAvailableJourneys()
 })
 </script>
 <template>
   <div class="user">
     <h2>行程信息</h2>
-    <el-form class="form">
-      <el-form-item label="集合地">
-        <el-input v-model="fromPos" />
-      </el-form-item>
-      <el-form-item label="目的地">
-        <el-input v-model="toPos" />
-      </el-form-item>
-      <el-form-item label="领队">
-        <el-input v-model="leader" />
-      </el-form-item>
-    </el-form>
+    <div class="journey-content-container" v-if="selectedJourney && Object.keys(selectedJourney).length>0">
+      <el-form class="form">
+        <el-form-item label="集合地">
+          <el-input v-model="selectedJourney.depaturePositionName" />
+        </el-form-item>
+        <el-form-item label="目的地">
+          <el-input v-model="selectedJourney.destinationPositionName" />
+        </el-form-item>
+        <el-form-item label="领队">
+          <el-input v-model="selectedJourney.leaderName" />
+        </el-form-item>
+        <el-form-item label="联系电话">
+          <el-input v-model="selectedJourney.leaderPhone" />
+        </el-form-item>
+      </el-form>
+      <div class="main-content">
+        <div>日程安排</div>
+        <el-input rows="6" type="textarea" v-model="selectedJourney.description" />
+      </div>
+    </div>
+    <p class="tip" v-else>选择出发日期后可以浏览行程明细</p>
     <h2>你的信息</h2>
-    <el-form :model="formData" class="form">
+    <el-form :model="formData" class="form" :rules="rules" ref="userFormRef">
       <el-form-item label="怎么称呼～" prop="name">
         <el-input v-model="formData.name"></el-input>
       </el-form-item>
       <el-form-item label="出发日期" prop="depatureDate">
         <el-date-picker
+          auto-focus
           :disabled-date="disabledDate"
           v-model="formData.depatureDate"
           @change="handleDateChange"
@@ -101,7 +137,7 @@ onMounted(() => {
           format="YYYY-MM-DD"
           value-format="YYYY-MM-DD">
         </el-date-picker>
-        <span>{{ selectedFee }} 元/人</span>
+        <span :style="{ color: 'lightcoral', marginLeft: '16px' }">{{ selectedFee }} 元/人</span>
       </el-form-item>
       <el-form-item label="出行人数" prop="headCount">
         <el-input-number v-model="formData.headCount"></el-input-number>
@@ -110,7 +146,7 @@ onMounted(() => {
         <el-input type="textarea" rows="5" v-model="formData.description"></el-input>
       </el-form-item>
       <el-form-item class="submit-btn">
-        <el-button @click="onSubmit" type="primary">提交</el-button>
+        <el-button @click="onSubmit" type="primary">报名！</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -120,6 +156,7 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+  // background-color: lightyellow;
   .submit-btn {
     :deep(.el-form-item__content) {
       display: flex;
@@ -135,5 +172,22 @@ onMounted(() => {
 :deep(.el-form-item__label) {
   width: 90px;
   text-align: right;
+}
+.tip {
+  color: lightcoral;
+}
+.journey-content-container {
+  display: flex;
+  flex-direction: row;
+  .form {
+    margin-right: 16px;
+    width: 40%;
+  }
+  .main-content {
+    width: 360px;
+    > div {
+      margin-bottom: 28px;
+    }
+  }
 }
 </style>
