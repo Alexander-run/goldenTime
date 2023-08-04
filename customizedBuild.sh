5a1276a345784ec4e84cb63df62d5f3f762f266a
Alexander-run <advance@AdvancedeMacBook-Pro.local>
2023-08-04 11:42:44 +0800

    u

--- customizedBuild.sh
+++ customizedBuild.sh
@@ -7,4 +7,4 @@ mkdir -p horse/apiservice
 mv -f frontend/admin-client/dist/* horse/admin
 mv -f frontend/user-client/dist/* horse/user
 mv -f service/dist/* horse/apiservice
-mv -f prod_pm2_config/* horse
+cp -f prod_pm2_config/* horse
